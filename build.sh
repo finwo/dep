38999e0e89217c0ac0887c1e28949fc3dbdf343c
Yersa Nordman <finwo@pm.me>
2019-11-05 20:47:58 +0100

    Added a preprocessor

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Load pre-processor
+[ -f bashpp ] || {
+  wget https://raw.githubusercontent.com/iwonbigbro/bashpp/master/bin/bashpp
+  chmod +x bashpp
+}
+
+# Refresh dist dir
+rm -rf dist
+mkdir dist
+
+# Build outputs
+./bashpp src/dep      > dist/dep
+./bashpp src/dep-repo > dist/dep-repo
+chmod +x dist/dep
+chmod +x dist/dep-repo
