50403f9f671a396a86ca9f4659d4443ccec8430f
Yersa Nordman <finwo@pm.me>
2019-11-05 20:49:59 +0100

    Use the right path in install.sh

--- install.sh
+++ install.sh
@@ -15,28 +15,19 @@ cat <<EOF
 
 EOF
 
-# Add system package
-function system {
-  command -v apt          &>/dev/null && { apt     install "$1" -y -qq; return; }
-  command -v apt-get      &>/dev/null && { apt-get install "$1" -y -qq; return; }
-  command -v apk          &>/dev/null && { apk     add     "$1"       ; return; }
-  command -v xbps-install &>/dev/null && { xbps-install    "$1"       ; return; }
-  echo "No supported package manager detected"
-  echo "Please install '$1' to run this script"
-  exit 1
-}
-
 # Install repo binary
 function bin {
   [ -f "./$1" ] && {
     echo "Copying $1"
     cp "./$1" "/usr/local/bin/$1"
-  } || curl -L# "https://raw.githubusercontent.com/cdeps/dep/master/$1" > "/usr/local/bin/$1"
+  } || curl -L# "https://raw.githubusercontent.com/cdeps/dep/master/dist/$1" > "/usr/local/bin/$1"
   chmod +x "/usr/local/bin/$1"
 }
 
 # Ensure our dependencies
-command -v curl &>/dev/null || system curl
+command -v curl &>/dev/null || {
+  echo "cUrl needs to be installed to use dep"
+}
 
 # Default install directory
 mkdir -p /usr/local/bin
