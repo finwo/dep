9c6a1ca8d23178e8bb4a8395c8d3a7f0787fa59f
finwo <finwo@pm.me>
2023-01-25 22:03:16 +0100

    Split commands into src subdir; replaced bashpp by preprocess

--- src/main.sh
+++ src/main.sh
@@ -1,17 +1,41 @@
-#include "ini.sh"
+cmds=("")
+# #include "command/help/index.sh"
+# #include "command/install.sh"
 
-# Filled by bashpp
+# Filled by preprocess
 NAME=__NAME
 
 function main {
+  cmd=help
 
-  # Commands:
-  #   install
+  while [ "$#" -gt 0 ]; do
 
+    # If argument is a command, pass parsing on to it & stop main parser
+    if [[ " ${cmds[*]} " =~ " $1 " ]]; then
+      cmd=$1
+      shift
+      arg_$cmd "$@"
+      break
+    fi
 
-  echo "Hello World"
+    # Main parser
+    case "$1" in
+      --)
+        shift
+        break 2
+        ;;
+      *)
+        echo "Unknown argument: $1" >&2
+        exit 1
+        ;;
+    esac
+    shift
+
+  done
+
+  cmd_$cmd
 }
 
 if [ $(basename $0) == "${NAME}" ]; then
-  main
+  main "$@"
 fi
---
+++ src/util/shopt.sh
@@ -0,0 +1,8 @@
+# #ifndef __SHOPT_SH__
+# #define __SHOPT_SH__
+
+# Required for the whitespace trimming
+shopt -s extglob
+
+# __SHOPT_SH__
+# #endif
