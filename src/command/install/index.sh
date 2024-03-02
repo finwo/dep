2d35f5797960d20e84b9fd13ef8e05f47964fd7c
Yersa Nordman <finwo@pm.me>
2024-03-02 22:58:54 +0100

    Added support for github-based packages from the repository

--- src/command/install/index.sh
+++ src/command/install/index.sh
@@ -5,6 +5,9 @@ read -r -d '' help_topics[install] <<- EOF
 # #include "help.txt"
 EOF
 
+CMD_INSTALL_PKG_NAME=
+CMD_INSTALL_PKG_DEST="lib"
+
 function arg_i {
   arg_install "$@"
   return $?
@@ -26,156 +29,149 @@ function cmd_install {
     exit 1
   fi
 
-  ini_foreach cmd_install_parse_ini_main "${PACKAGE_PATH}"
-  cmd_install_execute
+  # Fetch where to install dependencies
+  CMD_INSTALL_PKG_DEST=$(ini_foreach ini_output_value "${PACKAGE_PATH}" "package.deps")
+  if [ -z "${CMD_INSTALL_PKG_DEST}" ]; then CMD_INSTALL_PKG_DEST="lib"; fi
+
+  # Reset working directory
+  rm -rf "${CMD_INSTALL_PKG_DEST}/.__NAME"
+  mkdir -p "${CMD_INSTALL_PKG_DEST}/.__NAME/include"
+  echo "INCLUDES+=-I${CMD_INSTALL_PKG_DEST}/.__NAME/include" > "${CMD_INSTALL_PKG_DEST}/.__NAME/config.mk"
+  echo "" > "${CMD_INSTALL_PKG_DEST}/.__NAME/exported"
+
+  # Install all dependencies
+  ini_foreach cmd_install_parse_ini "${PACKAGE_PATH}"
   echo "Done"
 }
 
 cmds[${#cmds[*]}]="i"
 cmds[${#cmds[*]}]="install"
 
-CMD_INSTALL_PKG_NAME=
-CMD_INSTALL_PKG_DEST="lib"
 declare -A CMD_INSTALL_DEPS
-function cmd_install_parse_ini_main {
+function cmd_install_parse_ini {
   case "$1" in
-    package.)
-      case "$2" in
-        name)
-          CMD_INSTALL_PKG_NAME="$3"
-          ;;
-        deps)
-          CMD_INSTALL_PKG_DEST="$3"
-          ;;
-      esac
-      ;;
     dependencies.)
-      CMD_INSTALL_DEPS["$2"]="$3"
+      cmd_install_dep "$2" "$3"
       ;;
   esac
+
+    # package.)
+    #   case "$2" in
+    #     name)
+    #       CMD_INSTALL_PKG_NAME="$3"
+    #       ;;
+    #     deps)
+    #       CMD_INSTALL_PKG_DEST="$3"
+    #       ;;
+    #   esac
+    #   ;;
+    # dependencies.)
+    #   CMD_INSTALL_DEPS["$2"]="$3"
+    #   ;;
+  # esac
+
 }
 
-function cmd_install_execute {
-  cmd_install_reset_generated
-  for key in "${!CMD_INSTALL_DEPS[@]}"; do
-    cmd_install_dep "$key" "${CMD_INSTALL_DEPS[$key]}"
-  done
-}
-
-function cmd_install_reset_generated {
-  rm -rf "${CMD_INSTALL_PKG_DEST}/.__NAME"
-  mkdir -p "${CMD_INSTALL_PKG_DEST}/.__NAME/include"
-  echo "INCLUDES+=-I${CMD_INSTALL_PKG_DEST}/.__NAME/include" > "${CMD_INSTALL_PKG_DEST}/.__NAME/config.mk"
-  echo "" > "${CMD_INSTALL_PKG_DEST}/.__NAME/exported"
-}
+# function cmd_install_execute {
+#   cmd_install_reset_generated
+#   for key in "${!CMD_INSTALL_DEPS[@]}"; do
+#     cmd_install_dep "$key" "${CMD_INSTALL_DEPS[$key]}"
+#   done
+# }
 
 function cmd_install_dep {
-  local name=$1
-  local origin=$2
+  PKGNAME=$1
+  PKGVER=$2
 
-  # Full install if missing
-  local ISNEW=
-  if [ ! -d "${CMD_INSTALL_PKG_DEST}/${name}" ]; then
-    ISNEW="yes"
-
-    # Fetch package.ini for the dependency
-    mkdir -p "${CMD_INSTALL_PKG_DEST}/${name}"
-    case "${origin##*.}" in
-      ini)
-        # Download the package.ini for the dependency
-        curl --location --progress-bar "${origin}" --output "${CMD_INSTALL_PKG_DEST}/${name}/package.ini"
-        ;;
-      *)
-        # Download the assumed tarball
-        mkdir -p "${CMD_INSTALL_PKG_DEST}/.__NAME/cache/${name}"
-        if [ ! -f "${CMD_INSTALL_PKG_DEST}/.__NAME/cache/${name}/tarball-pkg" ]; then
-          curl --location --progress-bar "${origin}" --output "${CMD_INSTALL_PKG_DEST}/.__NAME/cache/${name}/tarball-pkg"
-        fi
-        # Extract tarball
-        tar --extract --directory "${CMD_INSTALL_PKG_DEST}/${name}/" --strip-components 1 --file="${CMD_INSTALL_PKG_DEST}/.__NAME/cache/${name}/tarball-pkg"
-        ;;
-    esac
-
-    # Fetch it's src (if present)
-    SRC="$(ini_foreach ini_output_value "${CMD_INSTALL_PKG_DEST}/${name}/package.ini" package.src)"
-    if [ ! -z "${SRC}" ]; then
-
-      # Download
-      mkdir -p "${CMD_INSTALL_PKG_DEST}/.__NAME/cache/${name}"
-      if [ ! -f "${CMD_INSTALL_PKG_DEST}/.__NAME/cache/${name}/tarball-src" ]; then
-        curl --location --progress-bar "${SRC}" --output "${CMD_INSTALL_PKG_DEST}/.__NAME/cache/${name}/tarball-src"
-      fi
-
-      # Verify checksum
-      HASH="$(ini_foreach ini_output_value "${CMD_INSTALL_PKG_DEST}/${name}/package.ini" package.src-sha256)"
-      if [ ! -z "${HASH}" ] && [ "${HASH}" != "$(sha256sum "${CMD_INSTALL_PKG_DEST}/.__NAME/cache/${name}/tarball-src" | awk '{print $1}')" ]; then
-        echo "The tarball for '${name}' failed it's checksum!" >&2
-        exit 1
-      fi
-
-      # Extract tarball
-      tar --extract --directory "${CMD_INSTALL_PKG_DEST}/${name}/" --strip-components 1 --file="${CMD_INSTALL_PKG_DEST}/.__NAME/cache/${name}/tarball-src"
-    fi
-
-    # Handle fetching extra files
-    mkdir -p "${CMD_INSTALL_PKG_DEST}/.__NAME/cache/${name}/fetch"
-    while read line; do
-      filename=${line%%=*}
-      filesource=${line#*=}
-
-      # Download the extra file into cache
-      if [ ! -f "${CMD_INSTALL_PKG_DEST}/.__NAME/cache/${name}/fetch/${filename}" ]; then
-        curl --location --progress-bar "${filesource}" --create-dirs --output "${CMD_INSTALL_PKG_DEST}/.__NAME/cache/${name}/fetch/${filename}"
-      fi
-
-    done < <(ini_foreach ini_output_section "${CMD_INSTALL_PKG_DEST}/${name}/package.ini" "fetch.")
-
-    # Copy the extra files into the target directory
-    tar --create --directory "${CMD_INSTALL_PKG_DEST}/.__NAME/cache/${name}" "fetch" | \
-      tar --extract --directory "${CMD_INSTALL_PKG_DEST}/${name}" --strip-components 1
+  # Skip if already installed
+  if [ -d "${CMD_INSTALL_PKG_DEST}/${PKGNAME}" ]; then
+    return 0
   fi
 
-  # Download package's dependencies
-  while read line; do
-    depname=${line%%=*}
-    deplink=${line#*=}
-    cmd_install_dep "$depname" "$deplink"
-  done < <(ini_foreach ini_output_section "${CMD_INSTALL_PKG_DEST}/${name}/package.ini" "dependencies.")
+  # Fetch versioned ini
+  PKGINIB="${HOME}/.config/finwo/__NAME/packages/${PKGNAME}/package.ini"
+  PKGINIV="${HOME}/.config/finwo/__NAME/packages/${PKGNAME}/${PKGVER}/package.ini"
+  PKGINI=
+  if [ -f "${PKGINIB}" ]; then PKGINI="${PKGINIB}"; fi
+  if [ -f "${PKGINIV}" ]; then PKGINI="${PKGINIV}"; fi
+  if [ -z "${PKGINI}" ]; then
+    echo "No package configuration found for ${PKGNAME}" >&2
+    exit 1
+  fi
+
+  # Copy repository's config for the package
+  PKG_SRC=$(dirname "${PKGINI}")
+  PKG_DIR="${CMD_INSTALL_PKG_DEST}/${PKGNAME}"
+  mkdir -p "$(dirname "${PKG_DIR}")"
+  cp -r "${PKG_SRC}" "${PKG_DIR}"
+  PKGINI="${PKG_DIR}/package.ini"
+
+  # Extended fetching detection
+  PKG_GH=$(ini_foreach ini_output_value "${PKGINI}" "repository.github")
+  PKG_TARBALL=$(ini_foreach ini_output_value "${PKGINI}" "package.src")
+
+  # Fetch target tarball from github repo
+  if [ ! -z "${PKG_GH}" ]; then
+    URL_TAG="https://codeload.github.com/${PKG_GH}/tar.gz/refs/tags/${PKGVER}"
+    URL_BRANCH="https://codeload.github.com/${PKG_GH}/tar.gz/refs/heads/${PKGVER}"
+    CODE_TAG=$(curl -X HEAD --fail --dump-header - -o /dev/null "${URL_TAG}" 2>/dev/null | head -1 | awk '{print $2}')
+    CODE_BRANCH=$(curl -X HEAD --fail --dump-header - -o /dev/null "${URL_BRANCH}" 2>/dev/null | head -1 | awk '{print $2}')
+    if [ "${CODE_TAG}" == "200" ]; then
+      PKG_TARBALL="${URL_TAG}"
+    elif [ "${CODE_BRANCH}" == "200" ]; then
+      PKG_TARBALL="${URL_BRANCH}"
+    fi
+  fi
+
+  # Fetch configured or detected tarball
+  if [ ! -z "${PKG_TARBALL}" ]; then
+    # Downloads a tarball and extracts if over the package in our dependency directory
+    # TARBALL_FILE="${HOME}/.config/finwo/__NAME/cache/${PKGNAME}/${PKGVER}.tar.gz"
+    TARBALL_FILE="${CMD_INSTALL_PKG_DEST}/.__NAME/cache/${PKGNAME}/${PKGVER}.tar.gz"
+    mkdir -p "$(dirname "${TARBALL_FILE}")"
+    if [ ! -f "${TARBALL_FILE}" ]; then
+      curl --location --progress-bar "${PKG_TARBALL}" --output "${TARBALL_FILE}"
+    fi
+    tar --extract --directory "${PKG_DIR}/" --strip-components 1 --file="${TARBALL_FILE}"
+  fi
+
+  # Handle the package's dependencies
+  ini_foreach cmd_install_parse_ini "${PKGINI}" "dependencies."
 
   # Handle any global build-steps defined in the package.ini
-  if [ ! -z "$ISNEW" ]; then
-    while read line; do
-      buildcmd=${line#*=}
-      echo $buildcmd
-      bash -c "cd ${CMD_INSTALL_PKG_DEST}/${name} ; ${buildcmd}"
-    done < <(ini_foreach ini_output_section "${CMD_INSTALL_PKG_DEST}/${name}/package.ini" "build." | sort --human-numeric-sort)
-  fi
+  while read line; do
+    buildcmd=${line#*=}
+    echo $buildcmd
+    bash -c "cd '${PKG_DIR}' ; ${buildcmd}"
+  done < <(ini_foreach ini_output_section "${PKGINI}" "build." | sort --human-numeric-sort)
 
   # Handle any os-generic build-steps defined in the package.ini
-  if [ ! -z "$ISNEW" ]; then
-    while read line; do
-      buildcmd=${line#*=}
-      echo $buildcmd
-      bash -c "cd ${CMD_INSTALL_PKG_DEST}/${name} ; ${buildcmd}"
-    done < <(ini_foreach ini_output_section "${CMD_INSTALL_PKG_DEST}/${name}/package.ini" "build-$(ostype)." | sort --human-numeric-sort)
-  fi
+  while read line; do
+    buildcmd=${line#*=}
+    echo $buildcmd
+    bash -c "cd '${PKG_DIR}' ; ${buildcmd}"
+  done < <(ini_foreach ini_output_section "${PKGINI}" "build-$(ostype)." | sort --human-numeric-sort)
 
   # Build the package's exports
-  if ! grep "${name}" "${CMD_INSTALL_PKG_DEST}/.__NAME/exported" &>/dev/null ; then
-    echo "${name}" >> "${CMD_INSTALL_PKG_DEST}/.__NAME/exported"
+  if ! grep "${PKGNAME}" "${CMD_INSTALL_PKG_DEST}/.__NAME/exported" &>/dev/null ; then
+    echo "${PKGNAME}" >> "${CMD_INSTALL_PKG_DEST}/.__NAME/exported"
     while read line; do
       filetarget=${line%%=*}
       filesource=${line#*=}
       mkdir -p "$(dirname "${CMD_INSTALL_PKG_DEST}/.__NAME/${filetarget}")"
       case "${filetarget}" in
+        exported|cache/*)
+          # Blocked
+          ;;
         config.mk)
-          cat "${CMD_INSTALL_PKG_DEST}/${name}/${filesource}" | sed "s|__DIRNAME|${CMD_INSTALL_PKG_DEST}/${name}|g" >> "${CMD_INSTALL_PKG_DEST}/.__NAME/${filetarget}"
+          cat "${PKG_DIR}/${filesource}" | sed "s|__DIRNAME|${PKG_DIR}|g" >> "${CMD_INSTALL_PKG_DEST}/.__NAME/${filetarget}"
           ;;
         *)
           # ls -sf "${CMD_INSTALL_PKG_DEST}/${name}/${filesource}" "${CMD_INSTALL_PKG_DEST}/.__NAME/${filetarget}"
-          cp "${CMD_INSTALL_PKG_DEST}/${name}/${filesource}" "${CMD_INSTALL_PKG_DEST}/.__NAME/${filetarget}"
+          cp "${PKG_DIR}/${filesource}" "${CMD_INSTALL_PKG_DEST}/.__NAME/${filetarget}"
           ;;
       esac
-    done < <(ini_foreach ini_output_section "${CMD_INSTALL_PKG_DEST}/${name}/package.ini" "export.")
+    done < <(ini_foreach ini_output_section "${PKGINI}" "export.")
   fi
+
 }
