46fd5295feb0353eb47d72e110785bcc26f715c6
Yersa Nordman <finwo@pm.me>
2026-03-13 11:55:49 +0100

    Auto-formatting

--- src/command/license/main.c
+++ src/command/license/main.c
@@ -5,20 +5,19 @@
 #include "license.h"
 
 int cmd_license(int argc, const char **argv) {
-    (void)argc;
-    (void)argv;
-    const unsigned char *start = _binary_LICENSE_md_start;
-    const unsigned char *end   = _binary_LICENSE_md_end;
-    size_t len = end - start;
-    fwrite(start, 1, len, stdout);
-    return 0;
+  (void)argc;
+  (void)argv;
+  const unsigned char *start = _binary_LICENSE_md_start;
+  const unsigned char *end   = _binary_LICENSE_md_end;
+  size_t               len   = end - start;
+  fwrite(start, 1, len, stdout);
+  return 0;
 }
 
 void __attribute__((constructor)) cmd_license_setup() {
   struct cmd_struct *cmd = calloc(1, sizeof(struct cmd_struct));
-  cmd->next = commands;
-  cmd->fn = cmd_license;
-  cmd->cmd = "license";
-  commands = cmd;
+  cmd->next              = commands;
+  cmd->fn                = cmd_license;
+  cmd->cmd               = "license";
+  commands               = cmd;
 }
-
--- src/main.c
+++ src/main.c
@@ -3,10 +3,9 @@
 #include <string.h>
 
 #include "cofyc/argparse.h"
+#include "command/command.h"
 #include "erkkah/naett.h"
 #include "rxi/microtar.h"
-
-#include "command/command.h"
 
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof(x[0]))
 
@@ -17,68 +16,62 @@ static const char *const usages[] = {
     NULL,
 };
 
-int
-cmd_foo(int argc, const char **argv)
-{
-    printf("executing subcommand foo\n");
-    printf("argc: %d\n", argc);
-    for (int i = 0; i < argc; i++) {
-        printf("argv[%d]: %s\n", i, *(argv + i));
-    }
-    int force = 0;
-    int test = 0;
-    const char *path = NULL;
-    struct argparse_option options[] = {
-        OPT_HELP(),
-        OPT_BOOLEAN('f', "force", &force, "force to do", NULL, 0, 0),
-        OPT_BOOLEAN('t', "test", &test, "test only", NULL, 0, 0),
-        OPT_STRING('p', "path", &path, "path to read", NULL, 0, 0),
-        OPT_END(),
-    };
-    struct argparse argparse;
-    argparse_init(&argparse, options, usages, 0);
-    argc = argparse_parse(&argparse, argc, argv);
-    printf("after argparse_parse:\n");
-    printf("argc: %d\n", argc);
-    for (int i = 0; i < argc; i++) {
-        printf("argv[%d]: %s\n", i, *(argv + i));
-    }
-    return 0;
+int cmd_foo(int argc, const char **argv) {
+  printf("executing subcommand foo\n");
+  printf("argc: %d\n", argc);
+  for (int i = 0; i < argc; i++) {
+    printf("argv[%d]: %s\n", i, *(argv + i));
+  }
+  int                    force     = 0;
+  int                    test      = 0;
+  const char            *path      = NULL;
+  struct argparse_option options[] = {
+      OPT_HELP(),
+      OPT_BOOLEAN('f', "force", &force, "force to do", NULL, 0, 0),
+      OPT_BOOLEAN('t', "test", &test, "test only", NULL, 0, 0),
+      OPT_STRING('p', "path", &path, "path to read", NULL, 0, 0),
+      OPT_END(),
+  };
+  struct argparse argparse;
+  argparse_init(&argparse, options, usages, 0);
+  argc = argparse_parse(&argparse, argc, argv);
+  printf("after argparse_parse:\n");
+  printf("argc: %d\n", argc);
+  for (int i = 0; i < argc; i++) {
+    printf("argv[%d]: %s\n", i, *(argv + i));
+  }
+  return 0;
 }
 
-int
-cmd_bar(int argc, const char **argv)
-{
-    printf("executing subcommand bar\n");
-    for (int i = 0; i < argc; i++) {
-        printf("argv[%d]: %s\n", i, *(argv + i));
-    }
-    return 0;
+int cmd_bar(int argc, const char **argv) {
+  printf("executing subcommand bar\n");
+  for (int i = 0; i < argc; i++) {
+    printf("argv[%d]: %s\n", i, *(argv + i));
+  }
+  return 0;
 }
 
-int
-main(int argc, const char **argv)
-{
-    struct argparse argparse;
-    struct argparse_option options[] = {
-        OPT_HELP(),
-        OPT_END(),
-    };
-    argparse_init(&argparse, options, usages, ARGPARSE_STOP_AT_NON_OPTION);
-    argc = argparse_parse(&argparse, argc, argv);
-    if (argc < 1) {
-        argparse_usage(&argparse);
-        return -1;
-    }
+int main(int argc, const char **argv) {
+  struct argparse        argparse;
+  struct argparse_option options[] = {
+      OPT_HELP(),
+      OPT_END(),
+  };
+  argparse_init(&argparse, options, usages, ARGPARSE_STOP_AT_NON_OPTION);
+  argc = argparse_parse(&argparse, argc, argv);
+  if (argc < 1) {
+    argparse_usage(&argparse);
+    return -1;
+  }
 
-    /* Try to run command with args provided. */
-    struct cmd_struct *cmd = commands;
-    while(cmd) {
-        if (!strcmp(cmd->cmd, argv[0])) break;
-        cmd = cmd->next;
-    }
-    if (cmd) {
-        return cmd->fn(argc, argv);
-    }
-    return 0;
+  /* Try to run command with args provided. */
+  struct cmd_struct *cmd = commands;
+  while (cmd) {
+    if (!strcmp(cmd->cmd, argv[0])) break;
+    cmd = cmd->next;
+  }
+  if (cmd) {
+    return cmd->fn(argc, argv);
+  }
+  return 0;
 }
