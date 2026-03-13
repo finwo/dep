6f56d5bccfda5392198e75c3049a03cc93575daf
finwo <finwo@pm.me>
2026-03-13 12:15:57 +0100

    Allow command aliasing

--- src/command/license/main.c
+++ src/command/license/main.c
@@ -18,6 +18,7 @@ void __attribute__((constructor)) cmd_license_setup() {
   struct cmd_struct *cmd = calloc(1, sizeof(struct cmd_struct));
   cmd->next              = commands;
   cmd->fn                = cmd_license;
-  cmd->cmd               = "license";
+  static const char *license_names[] = { "license", NULL };
+  cmd->name = license_names;
   commands               = cmd;
 }
